e6bb38bbf3f344aa51a11e6b0f9ad5ea35a93578
Rey-17 <antonios1789@gmail.com>
2018-11-08 10:40:39 -0500

    Creación de modelos para el manejo de roles y permisos

---
+++ app/Role.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'display_name', 'description'];
+}
